e53d70829e4d829a5356a30659fa438166d6acc2
Harshwagh21 <harshwagh8411@gmail.com>
2024-06-23 02:18:20 +0530

    Add contact landlord functionality to listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
 import { Navigation } from 'swiper/modules';
@@ -13,6 +14,7 @@ import {
     FaParking,
     FaShare,
   } from 'react-icons/fa';
+import Contact from '../components/Contact';
 
 export default function Listing() {
     SwiperCore.use([Navigation]);
@@ -21,8 +23,9 @@ export default function Listing() {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
     const [copied, setCopied] = useState(false);
-
+    const [contact, setContact] = useState(false);
     const params = useParams();
+    const {currentUser} = useSelector((state)=> state.user);
 
     useEffect(()=>{
         setLoading(true);
@@ -45,6 +48,7 @@ export default function Listing() {
         }
         fetchLisitng();
     }, [params.listingId]);
+
   return (
     <main>
         {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
@@ -127,6 +131,10 @@ export default function Listing() {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
+                <button onClick={()=>setContact(true)} className="bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3">Contact landlord</button>
+            )}
+            {contact && <Contact listing={listing} />}
           </div>
         </>
         )}
