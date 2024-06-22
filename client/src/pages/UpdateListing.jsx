1dd01369b5d090699f69bdafe238a8238735799b
Harshwagh21 <harshwagh8411@gmail.com>
2024-06-23 01:17:54 +0530

    Complete Listing page

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -261,7 +261,10 @@ export default function CreateListing() {
               />
               <div className="flex flex-col items-center">
                 <p>Regular Price</p>
-                <span className="text-sm">(₹ / month)</span>
+                <span className='text-xs'>(₹ / month)</span>
+                {formData.type === 'rent' && (
+                  <span className='text-xs'>(₹ / month)</span>
+                )}
               </div>
             </div>
             {formData.offer && (
@@ -278,7 +281,10 @@ export default function CreateListing() {
               />
               <div className="flex flex-col items-center">
                 <p>Discounted price</p>
-                <span className="text-sm">(₹ / month)</span>
+                <span className='text-xs'>(₹ / month)</span>
+                  {formData.type === 'rent' && (
+                    <span className='text-xs'>(₹ / month)</span>
+                  )}
               </div>
             </div>
             )}
